11e2cfdae754f38641f22226021372c77cb678b5
davincecode <vncntybnz@gmail.com>
2022-04-06 19:42:14 +0000

    Update Admin Dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
 
   def show
+    @categories = Category.all
+    @category_count = Category.count
+    @product_count = Product.count
   end
 end

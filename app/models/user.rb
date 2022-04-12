98a417c08c044b01f854814c68dd1a0ede3dfa19
davincecode <vncntybnz@gmail.com>
2022-04-12 23:00:53 +0000

    Update user controller and minor fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, :uniqueness => {:case_sensitive => false}
+  validates :password, length: { minimum: 3 }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+
+    context "All fields properly filled out" do
+      it "Creates the user successfully" do
+        user = User.new
+        user.first_name = "Michael"
+        user.last_name = "Jordan"
+        user.email = "mjhoops@gmail.com"
+        user.password = "u2hd874353kj23hr"
+        user.password_confirmation = "u2hd874353kj23hr"
+        user.save
+        expect(user.errors.full_messages).to match_array([])
+      end
+    end
+
+  end
 end

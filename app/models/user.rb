afdaeb6e57362567b69d156c1bcc264f1c74a1dc
davincecode <vncntybnz@gmail.com>
2022-04-12 23:59:18 +0000

    Update database for minor fix and update user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :email, uniqueness: => {:case_sensitive => false}
+  validates :email, :uniqueness => {:case_sensitive => false}
   validates :password, length: { minimum: 5 }
   validates :password, presence: true
   validates :password, confirmation: true
